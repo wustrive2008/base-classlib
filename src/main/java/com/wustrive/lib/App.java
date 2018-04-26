656ec937e629ebb3337825d45c8a8361647f7733
wubaoguo <wustrive_2008@126.com>
2018-04-26 14:53:24 +0800

    项目初始化

---
+++ src/main/java/com/wustrive/lib/App.java
@@ -0,0 +1,13 @@
+package com.wustrive.lib;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
